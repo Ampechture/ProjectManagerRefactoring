2361c9e70ac5d869b7bc71bf5250ccdc0b42173f
rinagalinskaya <92266103+rinagalinskaya@users.noreply.github.com>
2023-12-15 19:43:23 +0700

    Update TaskController.java addSubtask and Task

--- src/main/java/com/springeducational/projectmanagerrefactoring/MainCode/entity/controllers/TaskController.java
+++ src/main/java/com/springeducational/projectmanagerrefactoring/MainCode/entity/controllers/TaskController.java
@@ -1,7 +1,6 @@
 package com.ngu.restclienttemplate.MainCode.entity.controllers;
 
 
-
 import com.ngu.restclienttemplate.MainCode.entity.*;
 import com.ngu.restclienttemplate.MainCode.entity.repository.TaskRepository;
 import com.ngu.restclienttemplate.auth.UserService;
@@ -10,12 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
-public class TaskController {
+public class TaskController1 {
 
     @Autowired
     private TaskRepository taskRepository;
@@ -32,5 +34,34 @@ public class TaskController {
         model.addAttribute("newTask", new Task());
         model.addAttribute("newSubtask", new Subtask());
         return "index";
+    }
+
+    @PostMapping("/addTask")
+    public String addTask(Task task) {
+        if (taskRepository.existsByprojectName(task.getProjectName())) {
+            return "WrongLoginData";
+        } else {
+            Long userId = userService.getUserId();
+            User user = userService.userFindById(userId);
+            task.setUsersIdentity(List.of(user));
+
+            taskRepository.save(task);
+
+
+            return "redirect:/";
+        }
+    }
+
+
+    @PostMapping("/addSubtask/{projectId}")
+    public String addSubtask(@PathVariable Long projectId, Subtask subtask) {
+        Optional<Task> taskOptional = taskRepository.findById(projectId);
+        if (taskOptional.isPresent()) {
+            Task task = taskOptional.get();
+            subtask.setProject(task);
+            task.getSubtasks().add(subtask);
+            taskRepository.save(task);
+        }
+        return "redirect:/project/{projectId}";
     }
 }
