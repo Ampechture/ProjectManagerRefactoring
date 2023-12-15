ec8f69f55a943d5ee826191b6bd6ece0f88f68b6
Ampechture <55424626+Ampechture@users.noreply.github.com>
2023-12-15 21:17:19 +0700

    Delete ProjectManagerRefactor directory

--- ProjectManagerRefactor/src/main/java/com/springeducational/projectmanagerrefactoring/repositories/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springeducational.projectmanagerrefactoring.repositories;
-
-import com.springeducational.projectmanagerrefactoring.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-
-}
